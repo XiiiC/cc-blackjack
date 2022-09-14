066c37ae113cbeee45bbe4fec852c34dc7aaca2b
Hardijs <hardijs2002@gmail.com>
2022-09-14 14:45:36 +0100

    image finding test

--- imageTest.lua
+++ imageTest.lua
@@ -1,3 +1,15 @@
 local nft = require "cc.image.nft"
-local image = assert(nft.load("data/example.nft"))
+local image = assert(nft.load("images/cards/clubs/C-10.nfp"))
 nft.draw(image, term.getCursorPos())
+
+-- function file_exists(name)
+--     local f=io.open(name,"r")
+--     if f~=nil then io.close(f) return true else return false end
+--  end
+-- 
+-- if file_exists("images/cards/clubs/C-10.nfp") then
+--     print("File exists")
+-- else
+--     print("File does not exist")
+-- 
+-- end
